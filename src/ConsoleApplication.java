5f2415dcd47a5dbc921072122e31ab93d6d9ef52
Alexia Aldea <alexia.elena.aldea@gmail.com>
2023-04-06 23:53:09 +0300

    stylish

--- src/ConsoleApplication.java
+++ src/ConsoleApplication.java
@@ -1,5 +1,4 @@
-import model.Category;
-import model.Product;
+
 import Business.Store;
 import java.util.Scanner;
 public class ConsoleApplication {
@@ -10,25 +9,14 @@ public class ConsoleApplication {
 
         System.out.println();
         System.out.println("Buna! Alege optiunea dorita, apasand tasta corespunzatoare: ");
-        System.out.println("1. Login as admin");
-        System.out.println("2. Login as customer");
-        System.out.println("3. Adauga o categorie in magazin");
-        System.out.println("4. Adauga un produs in magazin");
-        System.out.println("5. Afiseaza toate produsele din magazin");
-        System.out.println("6. Adauga un rating");
-        System.out.println("7. Adauga un produs in cosul de cumparaturi");
-        System.out.println("8. Sterge un produs din cosul de cumparaturi");
-        System.out.println("9. Adauga un cupon");
-        System.out.println("10. Aplicare reducere cupoane");
-        System.out.println("11. Vizualizeaza cosul de cumparaturi");
-        System.out.println("12. Adauga adresa de livrare");
-        System.out.println("13. Comanda produsele");
-        System.out.println("14. Afiseaza comanda produselor");
-        System.out.println("15. Afiseaza profil client");
-        System.out.println("16. Afiseaza profil admin");
-        System.out.println("17. Afiseaza categoriile");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("|                   1. Login as admin                                  |");
+        System.out.println("|                   2. Login as customer                               |");
+        System.out.println("|                   0. EXIT                                            |");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 //        System.out.println("18. Logout");
-        System.out.println("Ce vei alege? ^^");
+        System.out.println("|                   Ce vei alege? ^^                                   |");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 
     public static void main(String[] args){
@@ -38,74 +26,135 @@ public class ConsoleApplication {
         while(true){
             comenzi();
         Scanner sc = new Scanner(System.in);
-        Integer comanda = sc.nextInt();
+        int comanda = sc.nextInt();
         switch (comanda){
+            case 0:
+                System.out.println("                          Sayonara!                        ");
+                return;
             case 1:
                 Store.addAdmin();
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("Alege optiunea dorita, din nou, apasand tasta corespunzatoare: ");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("|                   1. Adauga o categorie in magazin                   |");
+                System.out.println("|                   2. Adauga un produs in magazin                     |");
+                System.out.println("|                   3. Afiseaza toate produsele din magazin            |");
+                System.out.println("|                   4. Afiseaza clientii din magazin                   |");
+                System.out.println("|                   5. Afiseaza categoriile                            |");
+                System.out.println("|                   0. EXIT                                            |");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("|                   Ce vei alege? ^^                                   |");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                int comandaAdmin=sc.nextInt();
+                switch (comandaAdmin){
+                    case 1:
+                        Store.addCategory();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 2:
+                        Store.addProduct();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 3:
+                        Store.displayProduct();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 4:
+                        Store.displayCustomer();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 5:
+                        Store.displayCategory();
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("-----------------------------Done!--------------------------------");
+                }
                 break;
             case 2:
                 Store.addCustomer();
-                break;
-            case 3:
-                Store.addCategory();
-                System.out.println("Done!");
-                break;
-            case 4:
-                Store.addProduct();
-                System.out.println("Done!");
-                break;
-            case 5:
-                Store.displayProduct();
-                System.out.println("Done!");
-                break;
-            case 6:
-                Store.addRating();
-                System.out.println("Done!");
-                break;
-            case 7:
-                Store.addProductCart();
-                System.out.println("Done!");
-                break;
-            case 8:
-                Store.deleteProductCart();
-                System.out.println("Done!");
-                break;
-            case 9:
-                Store.addCoupon();
-                System.out.println("Done!");
-                break;
-            case 10:
-                Store.AplicareReducere();
-                System.out.println("Done!");
-                break;
-            case 11:
-                Store.displayCart();
-                System.out.println("Done!");
-                break;
-            case 12:
-                Store.addShipping();
-                System.out.println("Done!");
-                break;
-            case 13:
-                Store.addOrder();
-                System.out.println("Done!");
-                break;
-            case 14:
-                Store.displayOrder();
-                System.out.println("Done");
-                break;
-            case 15:
-                Store.displayCustomer();
-                System.out.println("Done");
-                break;
-            case 16:
-                Store.displayAdmin();
-                break;
-            case 17:
-                Store.displayCategory();
+
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("Alege optiunea dorita, din nou, apasand tasta corespunzatoare: ");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("|                   1. Afiseaza toate produsele din magazin            |");
+                System.out.println("|                   2. Adauga un rating                                |");
+                System.out.println("|                   3. Adauga un produs in cosul de cumparaturi        |");
+                System.out.println("|                   4. Sterge un produs din cosul de cumparaturi       |");
+                System.out.println("|                   5. Adauga un cupon                                 |");
+                System.out.println("|                   6. Aplicare reducere cupoane                       |");
+                System.out.println("|                   7. Vizualizeaza cosul de cumparaturi               |");
+                System.out.println("|                   8. Adauga adresa de livrare                        |");
+                System.out.println("|                   9. Comanda produsele                               |");
+                System.out.println("|                   10. Afiseaza comanda produselor                    |");
+                System.out.println("|                   11. Afiseaza profil client                         |");
+                System.out.println("|                   12. Afiseaza profil admin                          |");
+                System.out.println("|                   13. Afiseaza categoriile                           |");
+                System.out.println("|                   0. EXIT                                            |");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("|                   Ce vei alege? ^^                                   |");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                int comandaCustomer = sc.nextInt();
+                switch (comandaCustomer){
+                    case 1:
+                        Store.displayProduct();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 2:
+                        Store.addRating();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 3:
+                        Store.addProductCart();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 4:
+                        Store.deleteProductCart();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 5:
+                        Store.addCoupon();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 6:
+                        Store.AplicareReducere();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 7:
+                        Store.displayCart();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 8:
+                        Store.addShipping();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 9:
+                        Store.addOrder();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 10:
+                        Store.displayOrder();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 11:
+                        Store.displayCustomer();
+                        System.out.println("-----------------------------Done!--------------------------------");
+                        break;
+                    case 12:
+                        Store.displayAdmin();
+                        break;
+                    case 13:
+                        Store.displayCategory();
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("!!!!!!!!!!!!!!!!!!!Comanda inexistenta!!!!!!!!!!!!!!!!!!!");
+                }
                 break;
             default:
-                System.out.println("Comanda inexistenta!");
+                System.out.println("!!!!!!!!!!!!!!!!!!!Comanda inexistenta!!!!!!!!!!!!!!!!!!!");
         }
 
 
