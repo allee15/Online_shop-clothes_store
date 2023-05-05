b282ee47e63f4326b17eedbbd301b5eeb7e711e6
Alexia Aldea <alexia.elena.aldea@gmail.com>
2023-05-05 14:31:57 +0300

    creating the jdbc database

--- src/JDBCDemo.java
+++ src/JDBCDemo.java
@@ -1,17 +1,29 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Driver;
+import java.sql.*;
 
 public class JDBCDemo {
-    public static void main(String[] args){
-        String url = "ldbc:mysql://localhost:3306/jdbcdemo";
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
+    public static void main(String[] args) throws SQLException{
 
-            Connection connection= DriverManager.getConnection();
-        }
-        catch (Exception e){
-            System.out.println(e);
+        Connection connection = null;
+        Statement statememt =null;
+        try{
+            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/barbieshop", "root", "Sparky2002!");
+
+            statememt = connection.createStatement();
+
+            String sql = "INSERT INTO admin" + "(role, status)" + "Values('owner','available')";
+
+            statememt.execute(sql);
+
+            System.out.println("Succes!");
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            if (statememt !=null){
+                statememt.close();
+            }
+            if (connection !=null){
+                connection.close();
+            }
         }
     }
 }
